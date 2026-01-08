936425c27cc7f66a4ecc0b9e2af81dba93984973
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2026-01-07 21:57:26 -0300

    feat: implementa o search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,12 @@ import '../assets/styles/searchBar.css'
 export default function SearchBar() {
 
     return (
-        <div>
+        <div className='search-container'>
+            <input type="text" placeholder='Buscar imagem' className='search-input'/>
 
+            <button className="search-button">
+                <i className='fa-solid fa-magnifying-glass'></i>
+            </button>
         </div>
     );
 }
