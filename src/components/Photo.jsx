5bf3dbe195c8c21c414027b8b34dffd4894dd018
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2026-01-07 23:39:03 -0300

    feat: implementa componente photo

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,10 +1,14 @@
 import '../assets/styles/photo.css'
 
-export default function Photo() {
+export default function Photo({imgUrl, imgTitle}) {
 
     return (
-        <div>
+        <div className='photo-container'>
+            <img src={imgUrl} alt={imgTitle} />
 
+            <div className="photo-title">
+                <h3>{imgTitle}</h3>
+            </div>
         </div>
     );
 }
